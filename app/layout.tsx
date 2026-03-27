31a70c350c60051c9dc0f5f44d41078d36b26de4
yuji nakamura <y.nakamura@kxt.co.jp>
2026-03-27 20:27:54 +0900

    fix add

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
   return (
     <html lang="ja" data-theme="light" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content={ADSENSE_ID} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
